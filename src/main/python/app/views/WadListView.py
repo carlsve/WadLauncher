30a5eba0370b93a1831dbf4dac6c13f9af2a1cf4
karlpet <karlpetter.svensson@gmail.com>
2021-05-25 10:44:20 +0200

    Only communicate to models via id across views.

--- src/main/python/app/views/WadListView.py
+++ src/main/python/app/views/WadListView.py
@@ -3,10 +3,11 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItemModel
 
 from app.helpers.StackedWidgetSelector import widget_changed, display_widget, WidgetIndices
-from app.helpers.WadItemFactory import make_wad_item, DATA_ROLE
+from app.helpers.WadItemFactory import make_wad_item
 from app.helpers.ContextMenuFactory import make_context_menu
 
 LIST_ITEM_FLAGS = Qt.ItemIsSelectable | Qt.ItemIsEnabled
+ID_ROLE = Qt.UserRole + 1
 
 class WadListView:
     def __init__(self, root, controller):
@@ -48,13 +49,14 @@ class WadListView:
     def open_menu(self, pos):
         if self.selected_item == None:
             return
-        item = self.selected_item.data(DATA_ROLE)
+        id = self.selected_item.data(ID_ROLE)
+        wad = self.wads.find(id)
 
-        wad_string = item.get('title') or item.get('name')
+        wad_string = wad.get('title') or wad.get('name')
         remove_wad_string = 'Remove ({})'.format(wad_string)
         def remove_wad():
             self.wadlist_model.removeRow(self.selected_item.row())
-            self.controller.remove_wad(item)
+            self.controller.remove_wad(id)
         menu_actions = [(remove_wad_string, remove_wad)]
 
         execute_menu = make_context_menu(self.wadlist, menu_actions)
@@ -68,7 +70,7 @@ class WadListView:
         item = self.wadlist_model.itemFromIndex(index)
         self.selected_item = item
 
-        self.wads.select_wad(item.data(DATA_ROLE)['id'])
+        self.wads.select_wad(item.data(ID_ROLE))
 
     def appendWad(self, wad):
         item = make_wad_item(wad, LIST_ITEM_FLAGS)
@@ -77,7 +79,7 @@ class WadListView:
     def remove_item(self, wad):
         for row in range(self.wadlist_model.rowCount()):
             item = self.wadlist_model.item(row)
-            if item and item.data(DATA_ROLE)['id'] == wad['id']:
+            if item and item.data(ID_ROLE) == wad['id']:
                 self.wadlist_model.removeRow(row)
 
     def import_wads(self, wads):
